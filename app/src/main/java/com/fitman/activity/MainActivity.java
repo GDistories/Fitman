2aae7bef61e352880545537d3e355dc090e95d1b
Distories <18663977305@163.com>
2022-05-17 09:38:46 +0800

    Create Project

---
+++ app/src/main/java/com/fitman/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.fitman.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.fitman.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
