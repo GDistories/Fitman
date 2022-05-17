39b5bf918976aa2c301413a5818d69b190f38d97
Distories <18663977305@163.com>
2022-05-17 09:44:48 +0800

    Add BaseActivity

--- app/src/main/java/com/fitman/BaseActivity.java
+++ app/src/main/java/com/fitman/BaseActivity.java
@@ -1,16 +1,14 @@
-package com.fitman.activity;
+package com.fitman;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.fitman.R;
-
-public class MainActivity extends AppCompatActivity {
+public class BaseActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_base);
     }
 }
