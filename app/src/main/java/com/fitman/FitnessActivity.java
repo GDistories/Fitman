2f0b72a4014157386e86c1b61685d00f06af35dd
Distories <18663977305@163.com>
2022-06-21 10:48:36 +0800

    Add 5 video

--- app/src/main/java/com/fitman/FitnessActivity.java
+++ app/src/main/java/com/fitman/FitnessActivity.java
@@ -11,9 +11,11 @@ import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 
+import cn.jzvd.Jzvd;
 import cn.jzvd.JzvdStd;
 
 public class FitnessActivity extends BaseActivity {
@@ -25,25 +27,122 @@ public class FitnessActivity extends BaseActivity {
         setContentView(R.layout.activity_fitness);
         showActionBar();
         showBackButton();
-        setActionBarTitle(getString(R.string.fitness_title_1));//TODO
-
-
         ImageView imageStart = findViewById(R.id.imageStart);
+
+        ImageView imageCover = findViewById(R.id.imageCover);
+        TextView fitness_title = findViewById(R.id.fitness_title);
+        TextView time_num = findViewById(R.id.time_num);
+        TextView like_num = findViewById(R.id.like_num);
+        TextView level = findViewById(R.id.level);
+        TextView description = findViewById(R.id.description);
+
         ConstraintLayout constraintLayout = findViewById(R.id.constraintLayout_video);
         jzvdStd = findViewById(R.id.jzvd_Video);
+        Integer index = getIntent().getIntExtra("index", 1);
 
-        imageStart.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                constraintLayout.setVisibility(View.VISIBLE);
-                initView();
-            }
-        });
+        switch (index){
+            case 1:
+                setActionBarTitle(getString(R.string.fitness_title_1));
+
+                imageCover.setImageResource(R.drawable.fitness1);
+                fitness_title.setText(getString(R.string.fitness_title_1));
+                time_num.setText("10");
+                like_num.setText("7530");
+                level.setText(getString(R.string.easy));
+                description.setText(getString(R.string.fitness_description_1));
+
+                imageStart.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        constraintLayout.setVisibility(View.VISIBLE);
+                        jzvdStd.setUp(getVideoUrl(1), getString(R.string.fitness_title_1));
+                        jzvdStd.posterImageView.setImageResource(R.drawable.fitness1);
+                    }
+                });
+                break;
+            case 2:
+                setActionBarTitle(getString(R.string.fitness_title_2));
+
+                imageCover.setImageResource(R.drawable.fitness2);
+                fitness_title.setText(getString(R.string.fitness_title_2));
+                time_num.setText("10");
+                like_num.setText("6315");
+                level.setText(getString(R.string.easy));
+                description.setText(getString(R.string.fitness_description_2));
+
+                imageStart.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        constraintLayout.setVisibility(View.VISIBLE);
+                        jzvdStd.setUp(getVideoUrl(2), getString(R.string.fitness_title_2));
+                        jzvdStd.posterImageView.setImageResource(R.drawable.fitness2);
+                    }
+                });
+                break;
+            case 3:
+                setActionBarTitle(getString(R.string.fitness_title_3));
+
+                imageCover.setImageResource(R.drawable.fitness3);
+                fitness_title.setText(getString(R.string.fitness_title_3));
+                time_num.setText("10");
+                like_num.setText("5391");
+                level.setText(getString(R.string.easy));
+                description.setText(getString(R.string.fitness_description_3));
+
+                imageStart.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        constraintLayout.setVisibility(View.VISIBLE);
+                        jzvdStd.setUp(getVideoUrl(3), getString(R.string.fitness_title_3));
+                        jzvdStd.posterImageView.setImageResource(R.drawable.fitness3);
+                    }
+                });
+                break;
+            case 4:
+                setActionBarTitle(getString(R.string.fitness_title_4));
+
+                imageCover.setImageResource(R.drawable.fitness4);
+                fitness_title.setText(getString(R.string.fitness_title_4));
+                time_num.setText("20");
+                like_num.setText("5150");
+                level.setText(getString(R.string.hard));
+                description.setText(getString(R.string.fitness_description_4));
+
+                imageStart.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        constraintLayout.setVisibility(View.VISIBLE);
+                        jzvdStd.setUp(getVideoUrl(4), getString(R.string.fitness_title_4));
+                        jzvdStd.posterImageView.setImageResource(R.drawable.fitness4);
+                    }
+                });
+                break;
+            case 5:
+                setActionBarTitle(getString(R.string.fitness_title_5));
+
+                imageCover.setImageResource(R.drawable.fitness5);
+                fitness_title.setText(getString(R.string.fitness_title_5));
+                time_num.setText("10");
+                like_num.setText("4939");
+                level.setText(getString(R.string.easy));
+                description.setText(getString(R.string.fitness_description_5));
+
+                imageStart.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        constraintLayout.setVisibility(View.VISIBLE);
+                        jzvdStd.setUp(getVideoUrl(5), getString(R.string.fitness_title_5));
+                        jzvdStd.posterImageView.setImageResource(R.drawable.fitness5);
+                    }
+                });
+                break;
+        }
+
+
     }
 
-    void initView() {
-        jzvdStd.setUp(getVideoUrl(1)
-                , "Test01");
+    void initView(Integer index, String title) {
+        jzvdStd.setUp(getVideoUrl(1), title);
         jzvdStd.posterImageView.setImageResource(R.drawable.fitness1);//TODO
     }
 
@@ -58,6 +157,15 @@ public class FitnessActivity extends BaseActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+    @Override
+    public void onPause() {
+        super.onPause();
+        Jzvd.releaseAllVideos();
+    }
+
+
+
+
 
     public String getVideoUrl(Integer index){
         switch (index){
--- app/src/main/java/com/fitman/FitnessFragment.java
+++ app/src/main/java/com/fitman/FitnessFragment.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -22,12 +23,6 @@ import cn.jzvd.JzvdStd;
 
 public class FitnessFragment extends Fragment {
 
-    private VideoView vv_fitness;
-    private Uri uri;
-    private MediaController mediaController;
-    private JzvdStd jzvdStd;
-
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,15 +31,57 @@ public class FitnessFragment extends Fragment {
     @Override
     public void onStart() {
         super.onStart();
-        initView();
+        ConstraintLayout fitness_1 = getActivity().findViewById(R.id.constraintLayoutFitness1);
+        ConstraintLayout fitness_2 = getActivity().findViewById(R.id.constraintLayoutFitness2);
+        ConstraintLayout fitness_3 = getActivity().findViewById(R.id.constraintLayoutFitness3);
+        ConstraintLayout fitness_4 = getActivity().findViewById(R.id.constraintLayoutFitness4);
+        ConstraintLayout fitness_5 = getActivity().findViewById(R.id.constraintLayoutFitness5);
 
-        Button btn_start = getActivity().findViewById(R.id.btn_starts);
-        btn_start.setOnClickListener(new View.OnClickListener() {
+        fitness_1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(getActivity(), FitnessActivity.class));
+                Intent intent = new Intent(getActivity(), FitnessActivity.class);
+                intent.putExtra("index", 1);
+                startActivity(intent);
             }
         });
+
+        fitness_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), FitnessActivity.class);
+                intent.putExtra("index", 2);
+                startActivity(intent);
+            }
+        });
+
+        fitness_3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), FitnessActivity.class);
+                intent.putExtra("index", 3);
+                startActivity(intent);
+            }
+        });
+
+        fitness_4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), FitnessActivity.class);
+                intent.putExtra("index", 4);
+                startActivity(intent);
+            }
+        });
+
+        fitness_5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), FitnessActivity.class);
+                intent.putExtra("index", 5);
+                startActivity(intent);
+            }
+        });
+
     }
 
     @Override
@@ -54,20 +91,13 @@ public class FitnessFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_fitness, container, false);
     }
 
-    void initView() {
-        jzvdStd = (JzvdStd) getActivity().findViewById(R.id.jzvdStd6);
-        jzvdStd.setUp("https://rr5---sn-4g5lznes.googlevideo.com/videoplayback?expire=1655778344&ei=yNewYt3kJ9ia1gLXsYPgBg&ip=154.194.9.13&id=o-AHAEMfNNVr20fhqHgrEahp5HotaM22DZREKVGnN4fjek&itag=22&source=youtube&requiressl=yes&mh=FK&mm=31%2C29&mn=sn-4g5lznes%2Csn-4g5edndz&ms=au%2Crdu&mv=m&mvi=5&pl=24&initcwndbps=607500&vprv=1&mime=video%2Fmp4&ratebypass=yes&dur=1153.056&lmt=1650796440930380&mt=1655756449&fvip=2&fexp=24001373%2C24007246%2C24208265&c=ANDROID&txp=4532434&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cvprv%2Cmime%2Cratebypass%2Cdur%2Clmt&sig=AOq0QJ8wRQIgQjkBVp4nGC3WJM5YnCH1afAR5wRffjPYp5TDokKTIm4CIQCTukTw20_jCEzmdq0-dl17flHKys3pIhUrN_HvVqxJ2w%3D%3D&lsparams=mh%2Cmm%2Cmn%2Cms%2Cmv%2Cmvi%2Cpl%2Cinitcwndbps&lsig=AG3C_xAwRQIhAMpEvZVK1zQom1RH0QMERYK3Lmq9NbX7W4QrfMxiuiWbAiBJUpPeNG9wKWd3tNZU0lBAc9vHw-1pHSo1DWCR8V0TVg%3D%3D&title=Y2Mate.is%20-%20But%20what%20is%20a%20neural%20network%20%20Chapter%201%2C%20Deep%20learning-aircAruvnKk-720p-1655756755719"
-                , "Test01");
-        Glide.with(this)
-                .load("https://i.ytimg.com/vi_webp/ZVnfjTVZrWY/maxresdefault.webp")
-                .into(jzvdStd.posterImageView);
-    }
 
-
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        Jzvd.releaseAllVideos();
-    }
+//    void initView() {
+//        jzvdStd = (JzvdStd) getActivity().findViewById(R.id.jzvdStd6);
+//        jzvdStd.setUp("https://rr5---sn-4g5lznes.googlevideo.com/videoplayback?expire=1655778344&ei=yNewYt3kJ9ia1gLXsYPgBg&ip=154.194.9.13&id=o-AHAEMfNNVr20fhqHgrEahp5HotaM22DZREKVGnN4fjek&itag=22&source=youtube&requiressl=yes&mh=FK&mm=31%2C29&mn=sn-4g5lznes%2Csn-4g5edndz&ms=au%2Crdu&mv=m&mvi=5&pl=24&initcwndbps=607500&vprv=1&mime=video%2Fmp4&ratebypass=yes&dur=1153.056&lmt=1650796440930380&mt=1655756449&fvip=2&fexp=24001373%2C24007246%2C24208265&c=ANDROID&txp=4532434&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cvprv%2Cmime%2Cratebypass%2Cdur%2Clmt&sig=AOq0QJ8wRQIgQjkBVp4nGC3WJM5YnCH1afAR5wRffjPYp5TDokKTIm4CIQCTukTw20_jCEzmdq0-dl17flHKys3pIhUrN_HvVqxJ2w%3D%3D&lsparams=mh%2Cmm%2Cmn%2Cms%2Cmv%2Cmvi%2Cpl%2Cinitcwndbps&lsig=AG3C_xAwRQIhAMpEvZVK1zQom1RH0QMERYK3Lmq9NbX7W4QrfMxiuiWbAiBJUpPeNG9wKWd3tNZU0lBAc9vHw-1pHSo1DWCR8V0TVg%3D%3D&title=Y2Mate.is%20-%20But%20what%20is%20a%20neural%20network%20%20Chapter%201%2C%20Deep%20learning-aircAruvnKk-720p-1655756755719"
+//                , "Test01");
+//        Glide.with(this)
+//                .load("https://i.ytimg.com/vi_webp/ZVnfjTVZrWY/maxresdefault.webp")
+//                .into(jzvdStd.posterImageView);
+//    }
 }
